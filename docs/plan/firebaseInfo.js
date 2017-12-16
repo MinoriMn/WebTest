35b5cee18447745feb66de9b10e71343b09d3656
mohupero <core.filemode>
2017-12-17 08:15:42 +0900

    afterAll

---
+++ docs/plan/firebaseInfo.js
@@ -0,0 +1,12 @@
+//セキュリティについて
+//http://testpy.hatenablog.com/entry/2016/09/18/093000
+
+var config = {
+  apiKey: "AIzaSyA3ZGglOeo0TrXhiQ9FQDUrTL73nYQeZqk",
+  authDomain: "thchofufes.firebaseapp.com",
+  databaseURL: "https://thchofufes.firebaseio.com",
+  projectId: "thchofufes",
+  storageBucket: "thchofufes.appspot.com",
+  messagingSenderId: "1021090255471"
+};
+firebase.initializeApp(config);
